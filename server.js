47957f43615c579cd5fa3bc37dea63db4304649b
Bhuvanesh Ram M <a560977@fmr.com>
2015-03-18 12:42:42 +0530

    Updating changes

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.all('/*', function(req, res, next) {
 // Only the requests that start with /api/v1/* will be checked for the token.
 // Any URL's that do not follow the below pattern should be avoided unless you 
 // are sure that authentication is not needed
-app.all('/api/v1/*', [require('./middlewares/validateRequest')]);
+app.all('/api/v1/*', [require('./server/middlewares/validateRequest')]);
  
-app.use('/', require('./routes'));
+app.use('/', require('./server/routes'));
  
 // If no route is matched by now, it must be a 404
 app.use(function(req, res, next) {
